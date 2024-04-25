49e632d443f579b7479528387b1be8c5ba9837c7
joseph-ensemble <159160958+joseph-ensemble@users.noreply.github.com>
2024-04-25 16:32:29 +0200

    fix(locations list): add missing suspense tag

--- app/(site)/locations/page.jsx
+++ app/(site)/locations/page.jsx
@@ -1,11 +1,14 @@
 import LocationsList from "@/components/locationsList.jsx";
 import { getLocations } from "@/sanity/sanity-utils.js";
+import { Suspense } from "react";
 
 export default async function Locations() {
     const locations = await getLocations();
     return (
         <div className="flex flex-col gap-4 relative w-full p-7">
-            <LocationsList locations={locations}/>
+            <Suspense>
+                <LocationsList locations={locations}/>
+            </Suspense>
         </div>
     )
 }
