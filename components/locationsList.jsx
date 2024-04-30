cd2e2422685ae3c2b78da28886c7a4cf3f56ae3d
joseph-ensemble <159160958+joseph-ensemble@users.noreply.github.com>
2024-04-30 16:52:26 +0200

    feat(locations): gallery exit animation

--- components/locationsList.jsx
+++ components/locationsList.jsx
@@ -4,6 +4,7 @@ import Image from "next/image.js";
 import { useRouter, useSearchParams } from "next/navigation.js";
 import { useEffect, useState } from "react";
 import Gallery from "./utils/gallery.jsx";
+import { AnimatePresence } from "framer-motion";
 
 export default function LocationsList({ locations }) {
     const router = useRouter();
@@ -61,7 +62,9 @@ export default function LocationsList({ locations }) {
                     </h2>
             ))}
             <Image src={visibleImage.thumbnailImg} alt={visibleImage.name} width={1000} height={1000} className="h-screen w-screen object-cover fixed top-0 left-0 z-0" />
-            {showGallery && selectedGallery && <Gallery location={selectedGallery} onClose={handleCloseGallery} />}
+            <AnimatePresence>
+                {showGallery && selectedGallery && <Gallery location={selectedGallery} onClose={handleCloseGallery} />}
+            </AnimatePresence>
         </div>
     )
 }
--- components/utils/gallery.jsx
+++ components/utils/gallery.jsx
@@ -29,45 +29,43 @@ export default function Gallery({location, onClose}) {
     console.log(location);
 
     return (
-        <div className="fixed top-0 left-0 w-screen h-screen backdrop-blur-sm bg-black bg-opacity-40 z-50">
-
-                <motion.div
-                    key={location._key}
-                    initial={{ opacity: 0, x: '100vw' }}
-                    animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0, x: '100vw' }}
-                    transition={{ 
-                        type: 'spring',
-                        mass: 1,
-                        stiffness: 80,
-                        damping: 20,
-                        duration: 0.6 
-                    }}
-                >
-                    <div ref={galleryRef} className="absolute top-0 right-0 w-5/6 h-screen overflow-y-scroll bg-white text-black">
-                        <div className="top-0 p-7 flex flex-col gap-7 pointer-events-none">
-                            <h3 className="text-3xl">
-                                {location.name}
-                            </h3>
-                            <div className="max-w-readable">
-                                <PortableText value={location.description} />
-                            </div>
-                        </div>
-                        <div className="flex flex-col absolute w-full">
-                            {location.gallery.map((photo) => (
-                                <Image
-                                    key={photo._key}
-                                    src={photo.url}
-                                    alt=""
-                                    width={500}
-                                    height={500}
-                                    className="w-full object-cover"
-                                />
-                            ))}
+        <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-40 z-50">
+            <motion.div
+                key="modal"
+                initial={{ opacity: 1, x: '100vw' }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 1, x: '100vw' }}
+                transition={{ 
+                    type: 'spring',
+                    mass: 1,
+                    stiffness: 80,
+                    damping: 20,
+                    // duration: 0.6 
+                }}
+            >
+                <div ref={galleryRef} className="absolute top-0 right-0 w-5/6 h-screen overflow-y-scroll bg-white text-black">
+                    <div className="top-0 p-7 flex flex-col gap-7 pointer-events-none">
+                        <h3 className="text-3xl">
+                            {location.name}
+                        </h3>
+                        <div className="max-w-readable">
+                            <PortableText value={location.description} />
                         </div>
                     </div>
-                </motion.div>
-
+                    <div className="flex flex-col absolute w-full">
+                        {location.gallery.map((photo) => (
+                            <Image
+                                key={photo._key}
+                                src={photo.url}
+                                alt=""
+                                width={500}
+                                height={500}
+                                className="w-full object-cover"
+                            />
+                        ))}
+                    </div>
+                </div>
+            </motion.div>
         </div>
     )
 }
